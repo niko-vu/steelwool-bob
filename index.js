cea908a037416fc583544e662f2aeffee5ff1a4c
fearoffun <480dunce@gmail.com>
2023-07-13 23:47:56 -0700

    updated files

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
+// const schema = require('');
+// const seeds = require('');
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'options',
-                message: 'What would you like to do?',
-                choices: [
-                    'View all employees',
-                    'Add employee',
-                    'Update an employee role',
-                    'View all roles',
-                    'Add a role',
-                    'Add a department',
-                    'Quit'
-                ],
-            },
-        ])
+// (async () => {
+//     try {
+//         const answers = await getAnswers();
+//     } catch (err) {
+//         console.error(err.message)
+//     }
+// });
+
+function ask() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'Add employee',
+                'Update an employee role',
+                'View all roles',
+                'Add a role',
+                'Add a department',
+                'Quit'
+            ]
+        }
+    ])
+    .then(answers => {
+        if (answers.options === 'Quit') {
+            console.log('You have quit.');
+        } else if (answers.options === 'View all employees') {
+            
+        }
+    })
 }
 
-// Function call to initialize app
-init();
+ask();
+
+// Original inquirer prompting function??
+// function init() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: 'list',
+//                 name: 'options',
+//                 message: 'What would you like to do?',
+//
+//             },
+//         ])
+// }
+
+
+
